843b12f0082bfb6cb47732486384b46a408e6def
Harrison Cook <HonsonCooky@gmail.com>
2025-07-14 21:16:58 +1200

    Start task building

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,16 @@
 const std = @import("std");
-const task = @import("./task.zig");
-
-fn evaluateArgs(arg: []const u8, args: *std.process.ArgIterator) !void {
-    std.debug.print("Evaluating {s}", .{arg});
-    if (args.next()) |next| std.debug.print("Next: {s}", .{next});
-}
+const tasks = @import("./tasks.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-
     const allocator = gpa.allocator();
+
     var args = try std.process.argsWithAllocator(allocator);
     defer args.deinit();
 
-    _ = args.skip(); // Ignore the file path.
+    _ = args.skip();
 
-    // while (args.next()) |arg| {
-    //     try evaluateArgs(arg, &args);
-    // }
+    var task_manager = tasks.TaskManager.init(allocator);
+    defer task_manager.deinit();
 }
